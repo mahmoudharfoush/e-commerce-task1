968b66c1c61c8ae459f78bfcc12e239a4b5bbbb0
mahmoudharfoush <mahmoudharfoush5320@gmail.com>
2023-12-08 17:00:05 +0200

    test

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../components/dashboard/navbar/Navbar.jsx'
-import Footer from '../components/dashboard/footer/footer.jsx'
+import Footer from '../components/dashboard/footer/Footer.jsx'
 import { Outlet } from 'react-router-dom'
 
 export default function DashboradLayout() {
