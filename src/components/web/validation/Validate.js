accdb4ba658b4aaf6630ec86237c52c035d91d86
mahmoudharfoush <mahmoudharfoush5320@gmail.com>
2023-12-31 14:27:47 +0200

    newtask

--- src/components/web/validation/Validate.js
+++ src/components/web/validation/Validate.js
@@ -4,3 +4,18 @@ export const registerSchema = yup.object({
   email:yup.string().required("email is required").email(),
   password:yup.string().required("password is required").min(3,"must be at least 3 char").max(30,"max is 30 char")
 })
+
+export const loginSchema = yup.object({
+  email:yup.string().required("email is required").email(),
+  password:yup.string().required("password is required").min(3,"must be at least 3 char").max(30,"max is 30 char")
+})
+
+export const sendcodeSchema = yup.object({
+  email:yup.string().required("email is required").email(),
+})
+
+export const ForgotPasswordSchema = yup.object({
+  code:yup.string().required("code is required").length(4,"4 char"),
+  email:yup.string().required("email is required").email(),
+  password:yup.string().required("password is required").min(3,"must be at least 3 char").max(30,"max is 30 char")
+})
